2a9e20a132f818e82ed2c70ed600a1ee79eed368
Gouri1803 <bodhangouri@gmail.com>
2026-08-14 21:27:55 +0530

    inserted values i.e from costumer table to person done by manipulating columns in customer as per person table! i.e null and unknown!!

--- insert.sql
+++ insert.sql
@@ -1,4 +1,13 @@
-insert into customers (id , first_name)
-values (10, 'felix')
+-- method 2 
+-- insert data from one table to another using select
 
-select * from customers
+--copy data from 'customers' table into 'persons'
+insert into person (id , person_name , birth_date , phone)
+select
+id,
+first_name,
+null,
+'unknown'--this cannot be null as per person columns
+from customers
+
+select *from person
